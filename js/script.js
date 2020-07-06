5294851dc2142f1f1e2313a680050536c581e435
tringa-prekorogja <t.prekorogja@gmail.com>
2020-07-06 13:04:58 +0200

    Add text field for other option

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const nameInput = document.querySelector('#name')
+const otherInput = document.querySelector('#other-title')
+const selectRole = document.querySelector('select')
 
 window.onload = (event) => {
     nameInput.focus();
   };
+
+
+otherInput.style.display = "none"
+
+selectRole.addEventListener('change', (e)=>{
+    console.log(e.target.value)
+   if(e.target.value == 'other') {
+    otherInput.style.display = "block"; 
+   } else {
+    otherInput.style.display = "none"; 
+   }
+})
+
+
