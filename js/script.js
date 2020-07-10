1bcc5dab80d748e37a2e34a1c7858590e908499d
tringa-prekorogja <t.prekorogja@gmail.com>
2020-07-10 09:51:54 +0200

    Add payment section

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ const nameInput = document.querySelector('#name')
 const otherInput = document.querySelector('#other-title')
 const selectRole = document.querySelector('select')
 const selectColor = document.querySelector('#color')
-//const shirtColors = document.querySelectorAll('#color option')
+const shirtColors = document.querySelectorAll('#color option')
 const shirtDesign = document.querySelector('#design')
+const registerForActivities = document.querySelector('.activities')
+const activities = document.querySelectorAll('.activities input')
+let totalActivityCost = 0;
 
+// set focus on the first field 
 
 window.onload = (event) => {
     nameInput.focus();
 };
+
+// Job role section
 
 otherInput.style.display = "none"
 
@@ -20,10 +26,7 @@ selectRole.addEventListener('change', (e) => {
     }
 })
 
-
-
-const shirtColors = document.querySelectorAll('#color option')
-console.log(shirtColors)
+//T-Shirt info section
 
 for (let i = 0; i < shirtColors.length; i++) {
     shirtColors[i].hidden = true;
@@ -66,6 +69,74 @@ shirtDesign.addEventListener('change', (e) => {
     }
 })
 
+// Register for activities
+
+const newParagraph = document.createElement('p');
+registerForActivities.appendChild(newParagraph);
+
+
+registerForActivities.addEventListener('change', (e) => {
+    const activity = e.target
+    const activityDataCost = parseInt(activity.getAttribute('data-cost'))
+
+
+    if (activity.checked) {
+        totalActivityCost += activityDataCost
+    } else {
+        totalActivityCost -= activityDataCost
+    }
+    const totalCostParagraph = document.querySelector('.activities p')
+    totalCostParagraph.textContent = ` Total: $ ${totalActivityCost}`
+
+    const activityDayAndTime = activity.getAttribute('data-day-and-time')
+
+    for (let i = 0; i < activities.length; i++) {
+        let currentActivity = activities[i].getAttribute('data-day-and-time');
+        if (activityDayAndTime === currentActivity && activity !== activities[i]) {
+            if (activity.checked) {
+                activities[i].disabled = true;
+            } else {
+                activities[i].disabled = false;
+            }
+        }
+
+    }
+
+
+})
+
+
+//Payment section
+
+const selectPayment = document.querySelector('#payment')
+const selectPaymentMethod = selectPayment.firstElementChild.hidden = true;
+const creditCardSection = document.querySelector('.credit-card');
+const payPalSection = document.querySelector('.paypal')
+const bitcoinSection = document.querySelector('.bitcoin')
+
+creditCardSection.selected=true;
+payPalSection.hidden=true;
+bitcoinSection.hidden=true;
+
+
+selectPayment.addEventListener('change', (e) => {
+    const paymentMethod = e.target.value
+    if (paymentMethod === 'credit card') {
+       creditCardSection.hidden = false;
+       payPalSection.hidden = true;
+       bitcoinSection.hidden = true; 
+    } else if (paymentMethod ==='paypal') {
+        payPalSection.hidden = false;
+        creditCardSection.hidden = true;
+        bitcoinSection.hidden = true; 
+    } else if (paymentMethod === 'bitcoin'){
+        bitcoinSection.hidden = false; 
+        payPalSection.hidden = true;
+        creditCardSection.hidden = true;
+             
+    }
+creditCardSection.selected= true;
+})
 
 
 
